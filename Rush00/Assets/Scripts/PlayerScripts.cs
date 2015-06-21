c259f6f21d1624d22e6d66a4a6376f4b7b1a64e6
Willy B <wburgos@student.42.fr>
2015-06-21 23:18:34 +0200

    tirs

--- Rush00/Assets/Scripts/PlayerScripts.cs
+++ Rush00/Assets/Scripts/PlayerScripts.cs
@@ -6,14 +6,16 @@ using System.Collections.Generic;
 public class PlayerScripts : MonoBehaviour {
 
 
-	public GameObject bodyrotation; 
+	public GameObject bodyrotation;
 	public Camera cam;
 	bool equiped = false;
 	bool life = true;
 
 	float fAngle;
+	int number;
 
 	public List<GunsScripts> liste = new List<GunsScripts>();
+	public List<Sprite> bulletSprites = new List<Sprite>();
 	GunsScripts mygun;
 
 	public GameObject bullets;
@@ -21,25 +23,23 @@ public class PlayerScripts : MonoBehaviour {
 	public float fireRate = 0.02f;
 	private float nextFire;
 
+	public AudioSource fireSound;
+	public AudioSource equipSound;
+
 	Vector3 v3Pos;
 
 	#region triggercollision DropWeapon
-	
-	void OnTriggerEnter (Collider coll) {
-		if (coll.gameObject.tag == "Weapon") {
-			Debug.Log ("Catchable gun");
-		}
-	}
 
 	void OnTriggerStay (Collider coll) {
-		Debug.Log ("Ok");
 		if (coll.gameObject.tag == "Weapon" && equiped == false) {
 			if (Input.GetKeyDown (KeyCode.E)) {
 				coll.gameObject.SetActive(false);
 				equiped = true;
+				equipSound.Play();
 				for (int i = 0; i < liste.Count; i++) {
 					if (!liste [i].isActiveAndEnabled) {
 						mygun = liste [i];
+						number = i;
 						break;
 					}
 				}
@@ -49,50 +49,40 @@ public class PlayerScripts : MonoBehaviour {
 			life = false;
 		}
 	}
-	
-	void OnTriggerExit (Collider coll) {
-		if (coll.gameObject.tag == "Weapon") {
-			Debug.Log ("UnCatchable gun");
-		}
-	}
-	
+
 	void DropWeapon() {
 		for (int i = 0; i < liste.Count; i++) {
 			if (liste [i].isActiveAndEnabled)
 				continue;
 			else {
+				Vector3 tmp  = new Vector3(v3Pos.normalized.x, v3Pos.normalized.z, v3Pos.normalized.y);
 				liste [i].transform.position = transform.position;
-				liste [i].transform.position += v3Pos.normalized;
+				liste [i].transform.position += tmp;
 				liste [i].gameObject.SetActive (true);
-//				liste [i].transform.Translate (Vector3.up * Time.deltaTime);
-				liste [i].transform.Translate( v3Pos.normalized * Time.deltaTime );
+				liste [i].transform.Translate(tmp * Time.deltaTime );
 				equiped = false;
 			}
 		}
 	}
-		
+
 	#endregion
 
-	void Start () {
-
-	}
-
 	void Update () {
-	
+
 		movePlayer ();
 		if (Input.GetMouseButton (1) && equiped == true) {
 			DropWeapon();
 		}
 		if (Input.GetMouseButton (0) && equiped == true && Time.time > nextFire) {
 			nextFire = Time.time + fireRate;
-			GameObject obj = Instantiate (bullets, spwan.transform.position, Quaternion.identity) as GameObject;
+			GameObject obj = Instantiate (bullets);
+			obj.transform.position = spwan.transform.position;
+			bullets.GetComponent<SpriteRenderer>().sprite = bulletSprites[number];
 			obj.GetComponent<SpriteRenderer>().enabled = true;
 			obj.AddComponent<BulletScripts>();
-			Debug.Log (v3Pos.normalized);
 			Vector3 tmp  = new Vector3(v3Pos.normalized.x, v3Pos.normalized.z, v3Pos.normalized.y);
 			obj.GetComponent<BulletScripts>().direction = tmp;
-			Debug.Log (v3Pos.normalized);
-				//		audio.Play();
+			fireSound.Play();
 		}
 
 		if (life == false)
@@ -117,16 +107,10 @@ public class PlayerScripts : MonoBehaviour {
 			transform.Translate(Vector3.right * Time.deltaTime * 5);
 		}
 
-//		Debug.Log (bodyrotation.transform.rotation);
-
 		v3Pos = Camera.main.WorldToScreenPoint(bodyrotation.transform.position);
 		v3Pos = Input.mousePosition - v3Pos;
-//		Debug.Log ("v3Pos" + v3Pos);
 		fAngle = Mathf.Atan2 (v3Pos.y, v3Pos.x)* Mathf.Rad2Deg;
 		if (fAngle < -90.0f) fAngle += 360.0f;
-	
-//		Debug.Log (fAngle);
-
 		if (bodyrotation.transform.localRotation.z <= 270f && bodyrotation.transform.localRotation.z >= -90f) {
 			bodyrotation.transform.localEulerAngles = new Vector3 ( 0f, 0f, fAngle - 90f);
 		}
