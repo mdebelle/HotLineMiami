c20c26767ef2ef46bb096b0abbef0dbca774fea6
Marion DEBELLEY <mdebelle@e2r10p11.42.fr>
2015-06-21 20:56:42 +0200

    Shoot

--- Rush00/Assets/Scripts/GunsScripts.cs
+++ Rush00/Assets/Scripts/GunsScripts.cs
@@ -4,13 +4,17 @@ using System.Collections;
 public class GunsScripts : MonoBehaviour {
 
 
+	public BulletScripts bullet;
+	bool shoot;
+
 	// Use this for initialization
 	void Start () {
-	
+		shoot = false;
+		bullet.gameObject.SetActive (false);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
 	}
 }
